7bd840a67898a4b965838b6f7cf0a75257dfaf16
Anupam Kumar <anupam2221@yahoo.in>
2015-06-16 13:05:06 +0100

    Create tweetdatabase.py

---
+++ tweetdatabase.py
@@ -0,0 +1,18 @@
+import tweepy
+import time
+import os, random 
+
+ckey='add consumer key'
+csecret='consumer secret'
+akey='access token'
+asecret='access secret'
+
+auth=tweepy.OAuthHandler(ckey, csecret)
+auth.set_access_token(akey, asecret)
+x=1  
+file = open('ur file in which contents are listed','r')
+for line in file :
+        s = line 
+	api=tweepy.API(auth)
+	api.update_status(status=s)
+	time.sleep(5) #as per convinience
